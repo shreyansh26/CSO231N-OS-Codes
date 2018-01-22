c2406064b9ab11b94d567f82facd99564e1faecd
Shreyansh <shreyansh.pettswood@gmail.com>
2018-01-22 17:30:00 +0530

    Simple fork problem

---
+++ Practice/Fork/fork6.c
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int value = 5;
+
+int main() {
+	pid_t pid;
+	pid = fork();
+
+	if(pid == 0) {
+		value += 15;
+		return 0;
+	}
+	else if(pid > 0) {
+		wait(NULL);
+		printf("Parent value: %d\n", value);
+		return 0;
+	}
+}
